ebf487a243786a6aeb7e6d247a3c25381eb7424b
mariniertje <mariniertje@gmail.com>
2017-03-15 16:30:25 -0700

    Updated version #

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 here = path.abspath(path.dirname(__file__))
 
@@ -16,16 +16,16 @@ install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(name='pyadtpulse',
-      version='0.0.1',
-      description='Library to interface with portal.adtpulse.com accounts',
-      long_description=long_description,
-      url='http://github.com/mariniertje/pyadtpulse',
-	  download_url='https://github.com/mariniertje/pyadtpulse/archive/' + __version__,
-      author='Jeroen Goddijn',
-      author_email='mariniertje@gmail.com',
-      license='MIT',
-      packages=find_packages(exclude=['docs', 'tests*']),
-      include_package_data=True,
-      install_requires=install_requires,
-      dependency_links=dependency_links,
-      zip_safe=True)
+      version = __version__,
+      description = 'Library to interface with portal.adtpulse.com accounts',
+      long_description = long_description,
+      url = 'http://github.com/mariniertje/pyadtpulse',
+      download_url = 'https://github.com/mariniertje/pyadtpulse/archive/' + __version__,
+      author = 'Jeroen Goddijn',
+      author_email = 'mariniertje@gmail.com',
+      license = 'MIT',
+      packages = find_packages(exclude=['docs', 'tests*']),
+      include_package_data = True,
+      install_requires = install_requires,
+      dependency_links = dependency_links,
+      zip_safe = True)
